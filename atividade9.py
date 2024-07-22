2254e7d5334391246f1fe9fc490f37fc46d42f1f
Maarhs <marssouy1@gmail.com>
2024-07-22 15:50:54 -0300

    Delete atividade9.py

--- atividade9.py
+++
@@ -1,8 +0,0 @@
-while True:
-    nome = input("digite seu nome: ")
-    senha = input("digite sua senha: ")
-    if nome != senha:
-        print("Nome e senha valido")
-        break
-    else:
-        print("Nome e senha Não podem ser Iguais!!")
